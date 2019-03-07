ac2316501a48c67ee17f436be4ac7b26e31271f0
Kyle Mills <kyle.mills@uoit.net>
2019-03-07 12:34:39 -0800

    packaged it up

---
+++ plot1qbit/initiate.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rcParams
+import os
+
+here=os.path.abspath(__file__).replace("initiate.py","")
+
+font_files = font_manager.findSystemFonts(fontpaths=[os.path.join(here, "./")])
+font_list = font_manager.createFontList(font_files)
+font_manager.fontManager.ttflist.extend(font_list)
+plt.style.use(os.path.join(here, '1qbit.mplstyle'))
+
+
---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup( name='Plot1Qbit',
+       version='0.5',
+       packages=['plot1qbit'],
+       )
