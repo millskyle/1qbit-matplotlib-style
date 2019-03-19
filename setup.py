aa03b11d15e0a115cb67a40bed41f2a433e0753c
Kyle Mills <kyle.mills@uoit.net>
2019-03-19 11:45:25 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 
 setup( name='Plot1Qbit',
-       version='0.5',
+       version='0.6',
+       author='Kyle Mills',
+       author_email="kyle.mills@uoit.net",
+       description="Set matplotlib style to conform with 1QBit's brand policies",
        packages=['plot1qbit'],
+
        )
