c74c3d1d7a4348c775e36940ddfe953d47def7fe
Kyle Mills <kyle.mills@uoit.net>
2019-06-12 11:41:37 -0400

    incremented version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name='Plot1Qbit',
-       version='0.11',
+       version='0.12',
        author='Kyle Mills',
        author_email="kyle.mills@uoit.net",
        description="Set matplotlib style to conform with 1QBit's brand policies",
