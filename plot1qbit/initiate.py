48083b150223105edc5f338467bd7298c71d242d
Kyle Mills <kyle.mills@uoit.net>
2019-06-12 11:22:27 -0400

    fixed syntax error

--- plot1qbit/initiate.py
+++ plot1qbit/initiate.py
@@ -4,7 +4,7 @@ import os
 
 here=os.path.abspath(__file__).replace("initiate.py","")
 
-font_files = font_manager.findSystemFonts(fontpaths=[os.path.join(here, "./")])
+font_files = font_manager.findSystemFonts(fontpaths=[os.path.join(here, "./Brandon_Text")])
 font_list = font_manager.createFontList(font_files)
 font_manager.fontManager.ttflist.extend(font_list)
 plt.style.use(os.path.join(here, '1qbit.mplstyle'))
