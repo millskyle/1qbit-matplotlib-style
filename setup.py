bf21f0d33ed486a3cdc99ffdff348d73b3831bd4
Kyle Mills <kyle.mills@1qbit.com>
2023-02-21 18:16:14 +0000

    fixed missing package in setup.pu

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup( name='Plot1Qbit',
-       version='0.19',
+       version='0.20',
        author='Kyle Mills',
        author_email="kyle.mills@uoit.net",
        description="Set matplotlib style to conform with 1QBit's brand policies",
-       packages=['plot1qbit'],
+       packages=['plot1qbit', 'plot1qbit.report_maker'],
        install_requires=['tabulate', 'matplotlib>=3.1.0', 'gif', 'tqdm', 'numpy'],
        include_package_data=True
     )
